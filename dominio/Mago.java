3ab4a388456ee68c7c7742d668011a938d819cd5
tomaszepeda2411 <tomas.zepeda01@alumnos.ucn.cl>
2026-06-06 17:35:25 -0400

    Update Mago.java

--- dominio/Mago.java
+++ dominio/Mago.java
@@ -1,5 +1,27 @@
 package dominio;
+import java.util.ArrayList;
 
 public class Mago {
+	private String nombre;
+	private ArrayList<Hechizo> hechizosMago;
+	
+	public Mago(String nombre) {
+		
+		this.nombre = nombre;
+		this.hechizosMago = new ArrayList<>();
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void eliminarHechizo(Hechizo h) {
+		hechizosMago.remove(h);
+	}
+	public void agregarHechizo(Hechizo h) {
+		if (!hechizosMago.contains(h)) {
+			hechizosMago.add(h);
+		}
+	}
+	
 }
