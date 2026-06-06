df0531fc60d8d88ad4f89f1c3d43ad7282af7fb9
tomaszepeda2411 <tomas.zepeda01@alumnos.ucn.cl>
2026-06-06 17:35:49 -0400

    Update Hechizo.java

--- dominio/Hechizo.java
+++ dominio/Hechizo.java
@@ -1,5 +1,28 @@
 package dominio;
 
-public class Hechizo {
+public abstract class Hechizo {
+	protected String nombre;
+	protected String tipo;
+	protected int dano;
+	
+	public Hechizo(String nombre, String tipo, int dano) {
+		
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.dano = dano;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public int getDano() {
+		return dano;
+	}
+	
+	public abstract double calcularPuntuacion();
 }
